06f7b4b81b51d7fcfbeeb0d4769bc78fcd25a995
unknown <ariyeric@gmail.com>
2024-07-21 13:27:18 +0100

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './App.css'
+import Todo from './Components/Todo'
 
 function App() {
 
+  const [todo, settodo] = useState("")
+
   return (
     <>
-      <h1 className="text-3xl text-rose-700 font-bold underline">
-      Hello App!
-    </h1>
+      <div>
+        <div>
+          <h1>Todo List</h1>
+          <form>
+            <input type="text" />
+            <button>Add Todo</button>
+          </form>
+          <div className="todoDisplay">
+            <ul>
+              <li>Single Item</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/Components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <>
+        <div className='text-4xl text-center'>Todo List</div>
+    </>
+  )
+}
+
+export default Todo
