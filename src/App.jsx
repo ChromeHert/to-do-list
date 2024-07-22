41e02f34787f4e74613462be056e2e61349d0bb1
unknown <ariyeric@gmail.com>
2024-07-22 11:20:14 +0100

    Todo display array

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import { React, useState } from 'react'
+import { React } from 'react'
 import './App.css'
 import Todo from './Components/Todo'
 
 function App() {
 
-  const [todo, setTodo] = useState("")
-
   return (
-    <>
-      <div className='bg-gray-200 w-full h-screen flex items-center'>
-        <div className='w-[500px] mx-auto bg-white p-5'>
-          <h1 className='text-6xl font-bold text-center mb-8'>Todo List</h1>
-          <form>
-            <input type="text" className='border-2 border-gray-800 w-full p-3 placeholder:text-gray-600'
-            placeholder='Enter your tasks'
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
-            />
-
-            <button className='bg-blue-900 text-white py-3 px-8 rounded-lg mb-5' type="submit">Add Todo</button>
-          </form>
-          <div className="todoDisplay">
-            <ul>
-              <li className='bg-gray-800 text-white py-5 px-2 rounded-lg flex justify-between'>Single Item
-                <span className='text-red-600 cursor-pointer'>X</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
+    <Todo/>
   )
 }
 
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { React, useState } from 'react'
 
 const Todo = () => {
+
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    setTodoList([...todoList, {todoName: todo}]);
+    setTodo("");
+  };
   return (
     <>
-        <div className='text-4xl text-center'>Todo List</div>
+      <div className="bg-gray-200 w-full h-screen flex items-center">
+        <div className="w-[500px] mx-auto bg-white p-5 rounded-xl">
+          <h1 className="text-6xl font-bold text-center mb-8 text-gray-800">
+            Todo List
+          </h1>
+          <form onSubmit={handleForm}>
+            <input
+              type="text"
+              className="border-2 border-gray-800 w-full p-3 rounded-lg placeholder:text-gray-600"
+              placeholder="Enter your tasks"
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+            />
+
+            <button
+              className="bg-blue-900 text-white py-3 px-8 rounded-lg mb-5 my-4"
+              type="submit"
+            >
+              Add Todo
+            </button>
+          </form>
+          <div className="todoDisplay">
+            <ul>
+              {todoList.map((singleTodo, index) => {
+                return (
+                  <li key={index} className="bg-gray-800 text-white py-5 px-4 rounded-lg my-1 flex justify-between">
+                    {singleTodo.todoName}
+                    <span className="text-red-600 cursor-pointer">X</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Todo
